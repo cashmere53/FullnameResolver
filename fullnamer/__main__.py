4c259399d9b23c50c6dd7e656ee9b41e8de7bfb9
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2022-10-12 12:49:22 +0900

    separate logger setup process

--- fullnamer/__main__.py
+++ fullnamer/__main__.py
@@ -3,7 +3,6 @@
 import sys
 from os import fspath
 
-import loguru
 from loguru import logger
 
 from fullnamer.commandline import parse_input
@@ -12,22 +11,27 @@ from fullnamer.fullnamer import resolve_filepath
 
 def main() -> None:
     commandline_info = parse_input()
-
-    logger.remove()
-    logging_level: str = "SUCCESS"
-    if commandline_info.verbose == 1:
-        logging_level = "INFO"
-    elif commandline_info.verbose == 2:
-        logging_level = "DEBUG"
-    elif commandline_info.verbose >= 3:
-        logging_level = "TRACE"
-    logger.add(sys.stdout, level=logging_level)
+    setup_logger(commandline_info.verbose)
 
     logger.debug(f"{commandline_info}")
     resolve_path = resolve_filepath(commandline_info.path, check_exists=commandline_info.exist)
 
     for item in resolve_path:
         print(fspath(item))
+
+
+def setup_logger(verbosity: int = 0) -> None:
+    logger.remove()
+
+    logging_level: str = "SUCCESS"
+    if verbosity == 1:
+        logging_level = "INFO"
+    elif verbosity == 2:
+        logging_level = "DEBUG"
+    elif verbosity >= 3:
+        logging_level = "TRACE"
+
+    logger.add(sys.stderr, level=logging_level)
 
 
 if __name__ == "__main__":
